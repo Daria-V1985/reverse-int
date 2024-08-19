bf317994bdc0c4f02609ab31efa750d0b35666ca
Daria <darjana29@gmail.com>
2024-08-19 18:18:40 +0300

    fix: exchange method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-module.exports = function reverse (n) {
-  let result = 0;
-
-  while(n != 0 || n < 0) {
-    result = (result * 10) + (n % 10);
-    n = Math.floor(Math.abs(n) / 10);
-    
-  }
-  return result;
-}
+module.exports = reverse = (n) => 
+  n >= 0
+  ? +`${n}`.split('').reverse().join('')
+  : +(`${-n}`.split('').reverse().join(''));
