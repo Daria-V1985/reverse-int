b7b16e4f8eee6b4830e048957548b857fc76d671
Daria <darjana29@gmail.com>
2024-08-19 18:05:11 +0300

    fix: add method Math.abs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function reverse (n) {
   let result = 0;
 
-  while(n != 0) {
+  while(n != 0 || n < 0) {
     result = (result * 10) + (n % 10);
-    n = Math.floor(n / 10);
+    n = Math.floor(Math.abs(n) / 10);
     
   }
   return result;
